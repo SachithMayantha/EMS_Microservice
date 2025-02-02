314f90c25523d45c5cc88f6c8d1d5424aac283b2
SachithMayantha <sachithfernando599@gmail.com>
2025-02-02 19:26:51 +1100

    Spring WebClient

--- department-service/department-service/src/main/java/net/fernando/departmentservice/DepartmentServiceApplication.java
+++ department-service/department-service/src/main/java/net/fernando/departmentservice/DepartmentServiceApplication.java
@@ -2,10 +2,22 @@ package net.fernando.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
+//	@Bean
+//	public RestTemplate restTemplate(){
+//		return new RestTemplate();
+//	}
+
+	@Bean
+	public WebClient webClient(){
+		return WebClient.builder().build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
--- employee-service/employee-service/src/main/java/net/fernando/employeeservice/service/Impl/EmployeeServiceImpl.java
+++ employee-service/employee-service/src/main/java/net/fernando/employeeservice/service/Impl/EmployeeServiceImpl.java
@@ -11,6 +11,7 @@ import net.fernando.employeeservice.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -18,7 +19,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository repository;
 
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+//    private RestTemplate restTemplate;
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
 
@@ -31,9 +33,21 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public APIResponseDto getEmployee(Long employeeId) {
         Employee employee = repository.findById(employeeId).get();
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"+employee.getDepartmentCode(), DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+        // get() get the webclient instance
+        DepartmentDto departmentDto = webClient.get()
+                // call department application API
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                // fetch the response (body) from the server
+                .retrieve()
+                // convert (map) response body into single DepartmentDto Object
+                .bodyToMono(DepartmentDto.class)
+                // block the thread until the Mono completes and returns the actual value (DepartmentDto)
+                // Synchronous call
+                .block();
 
         EmployeeDto employeeDto = EmployeeMapper.mapToEmployeeDto(employee);
 
